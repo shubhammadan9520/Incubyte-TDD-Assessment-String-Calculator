5410a9ca11b3e4631923c3661ab55ef60f40a99d
Shubham <shubhammadan9520@gmail.com>
2025-08-07 00:51:32 +0530

    fixed tests for string with custom delimiter

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,17 @@
 function add(numbers) {
   if (numbers === "") return 0;
-  const delimiterRegex = /,|\n/;
+
+  let delimiterRegex = /,|\n/;
+
+  if (numbers.startsWith("//")) {
+    const match = numbers.match(/^\/\/(.)\n(.*)/);
+    if (match) {
+      const [, customDelim, rest] = match;
+      delimiterRegex = new RegExp(customDelim);
+      numbers = rest;
+    }
+  }
+
   const nums = numbers.split(delimiterRegex).map(Number);
   return nums.reduce((sum, n) => sum + n, 0);
 }
