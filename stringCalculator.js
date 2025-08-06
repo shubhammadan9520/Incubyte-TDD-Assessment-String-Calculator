8debffeab92ad5f1e899a3a321b29831d0cedab3
Shubham <shubhammadan9520@gmail.com>
2025-08-07 00:55:26 +0530

    Fixed tests for handling negative numbers and throw error

--- stringCalculator.js
+++ stringCalculator.js
@@ -13,6 +13,12 @@ function add(numbers) {
   }
 
   const nums = numbers.split(delimiterRegex).map(Number);
+
+  const negatives = nums.filter(n => n < 0);
+  if (negatives.length > 0) {
+    throw new Error(`negative numbers not allowed: ${negatives.join(",")}`);
+  }
+
   return nums.reduce((sum, n) => sum + n, 0);
 }
 
