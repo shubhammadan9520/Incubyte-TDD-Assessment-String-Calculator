43a44b57b590f1f18b606baf1968ad90801183ed
Shubham <shubhammadan9520@gmail.com>
2025-08-07 01:01:14 +0530

    Added failing test cases for string with numbers greater than 1000

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -40,3 +40,7 @@ test('throws exception on multiple negative numbers', () => {
   expect(() => add("1,-2,3,-4,-5")).toThrow("negative numbers not allowed: -2,-4,-5");
 });
 
+test('ignores numbers greater than 1000', () => {
+  expect(add("2,1001")).toBe(2);
+  expect(add("1000,1")).toBe(1001);
+});
